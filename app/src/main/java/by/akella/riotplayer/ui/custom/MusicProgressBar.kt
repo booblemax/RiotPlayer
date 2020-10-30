70870864cbed5cdfa07ab40793ba4936df06eafc
astapenko <astapenko>
2020-10-30 11:09:38 +0300

    refactor musicProgressbar to implement from seekbar, for can use in mini player fragment

--- app/src/main/java/by/akella/riotplayer/ui/custom/MusicProgressBar.kt
+++ app/src/main/java/by/akella/riotplayer/ui/custom/MusicProgressBar.kt
@@ -2,36 +2,57 @@ package by.akella.riotplayer.ui.custom
 
 import android.content.Context
 import android.util.AttributeSet
-import com.google.android.material.slider.LabelFormatter
-import com.google.android.material.slider.Slider
+import android.widget.SeekBar
+import androidx.appcompat.widget.AppCompatSeekBar
+import by.akella.riotplayer.R
+import kotlin.math.ceil
+import kotlin.math.floor
 
 class MusicProgressBar @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : Slider(context, attrs, defStyleAttr) {
+    defStyleAttr: Int = R.attr.seekBarStyle
+) : AppCompatSeekBar(context, attrs, defStyleAttr) {
 
     var onTouchEnds: (Int) -> Unit = {}
+
+    var valueFrom: Int = -1
+        set(value) {
+            if (value > valueTo) throw IllegalArgumentException("valueFrom must be less than valueTo")
+            field = value
+        }
+
+    var valueTo: Int = 0
+
+    var value: Int = 0
+        set(value) {
+            if (!isTrackingStarted) {
+                progress = ceil(value * DIVIDEND / getInterval()).toInt()
+            }
+            field = value
+        }
+        get() = ceil(progress * getInterval() / DIVIDEND).toInt()
 
     private var isTrackingStarted = false
 
     init {
-        labelBehavior = LabelFormatter.LABEL_GONE
-        addOnSliderTouchListener(object : OnSliderTouchListener {
-            override fun onStartTrackingTouch(slider: Slider) {
+        setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) { }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
                 isTrackingStarted = true
             }
 
-            override fun onStopTrackingTouch(slider: Slider) {
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
                 isTrackingStarted = false
-                onTouchEnds(value.toInt())
+                onTouchEnds(value)
             }
         })
     }
 
-    override fun setValue(value: Float) {
-        if (!isTrackingStarted) {
-            super.setValue(value)
-        }
+    private fun getInterval(): Int = valueTo - valueFrom
+
+    companion object {
+        private const val DIVIDEND = 100.0
     }
 }
