7aceaf73542586926a1de8aa24b69ebe88282094
astapenko <astapenko>
2020-11-05 11:43:16 +0300

    added ability to perform click on bar

--- app/src/main/java/by/akella/riotplayer/ui/custom/CircleProgressBar.kt
+++ app/src/main/java/by/akella/riotplayer/ui/custom/CircleProgressBar.kt
@@ -4,13 +4,20 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import by.akella.riotplayer.R
+import java.lang.Math.pow
+import java.lang.Math.sqrt
+import kotlin.math.acos
 import kotlin.math.ceil
 import kotlin.math.cos
 import kotlin.math.max
+import kotlin.math.pow
+import kotlin.math.round
 import kotlin.math.sin
+import kotlin.math.sqrt
 
 class CircleProgressBar @JvmOverloads constructor(
     context: Context,
@@ -86,6 +93,7 @@ class CircleProgressBar @JvmOverloads constructor(
         val top = paddingTop + pathWidth
         val end = width.toFloat() - pathWidth
         val bottom = height.toFloat() - pathWidth
+
         canvas?.drawArc(
             start, top, end, bottom,
             -90f, (progress / DIVIDEND * FULL_ANGLE).toFloat(), false, linePaint)
@@ -98,9 +106,40 @@ class CircleProgressBar @JvmOverloads constructor(
         thumbY = (measuredHeight / 2 + r * sin(rads)).toFloat()
     }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event ?: return super.onTouchEvent(event)
+
+        val x = event.x
+        val y = event.y
+
+        if (event.action == MotionEvent.ACTION_MOVE || isTouch(x, y)) {
+            val cos = computeCos(x, y)
+            val angle = if (x < width / 2) Math.PI * RADIAN + acos(cos) * RADIAN
+                        else Math.PI * RADIAN - acos(cos) * RADIAN
+            value = round(valueTo * angle / 360).toLong()
+
+            return true
+        }
+
+        return super.onTouchEvent(event)
+    }
+
+    private fun isTouch(x: Float, y: Float): Boolean {
+        val radius = (width - paddingStart - paddingEnd + pathWidth) / 2
+        return (centerX - x).pow(2) + (centerY - y).pow(2) < radius * radius
+    }
+
+    private fun computeCos(x: Float, y: Float): Float {
+        val w = x - width / 2
+        val h = y - height / 2
+        val slope = sqrt(w * w + h * h)
+        return h / slope
+    }
+
     companion object {
         private const val DIVIDEND = 100.0
         private const val FULL_ANGLE = 360f
         private const val HALF_ANGLE = 180f
+        private const val RADIAN = 180f / Math.PI
     }
 }
